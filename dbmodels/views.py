40e001241c2cd92360e2175aec8ad404cc4fb52d
juliloa <159180103+juliloa@users.noreply.github.com>
2025-04-15 03:15:56 -0500

    Update views.py

--- dbmodels/views.py
+++ dbmodels/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# NOSONAR: Ignora el warning de parámetro innecesario
 def home(request):
     return HttpResponse("Bienvenido al sistema LOGIVAG")
