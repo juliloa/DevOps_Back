a535ef0d32515263471cdd4cd45df3fb6e2af06d
juliloa <159180103+juliloa@users.noreply.github.com>
2025-05-28 00:41:46 -0500

    fix: settings.py

--- LOGIVAG/settings.py
+++ LOGIVAG/settings.py
@@ -31,7 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-dev-secret-key-only-for-local')
+
+if not SECRET_KEY or (SECRET_KEY == 'insecure-dev-secret-key-only-for-local' and not DEBUG):
+    raise Exception(" SECRET_KEY inválida o insegura usada en producción. Configura correctamente la variable de entorno.")
+
 
 DEBUG = 'RENDER' not in os.environ
 
